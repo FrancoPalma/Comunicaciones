8c8c1602ce57171c471dbdd4e24f639885ee5539
Francoco97 <fpalmatrejo@gmail.com>
2020-06-05 16:50:16 -0400

    ads

--- prueba.py
+++ prueba.py
@@ -1,1 +1,9 @@
-print("El diego es puto")
+import random
+import string
+for i in range(30):
+    palabra=""
+    for i in range(20):
+        palabra+=random.choice(string.ascii_lowercase)
+    file = open("D:/Github/archivo"+p+".txt", "w")
+    file.write(palabra)
+    file.close()
