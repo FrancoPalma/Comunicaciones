1718567043367e79610f1aab8960cd6da7895d11
dEmanu1001 <64873829+dEmanu1001@users.noreply.github.com>
2020-06-06 18:36:38 -0400

    s

--- prueba.py
+++ prueba.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import sys
+import ImageChops
 try:
     img = Image.open("descarga.jpg")
+    img2 = Image.open("paisaje.png")
 except:
     print("No se pudo cargar la imagen")
     sys.exit(1)
 #img.show()
 #Conversión de JPG a PNG
-img.save("paisaje.gif","gif")
+#img.save("paisaje.gif","gif")
 #Rotación de imágenes
 #img2 = img.rotate(45)
 #img2.show()
@@ -15,6 +17,8 @@ img.save("paisaje.gif","gif")
 ancho,alto = img.size
 print("Ancho: ",ancho)
 print("Alto: ",alto)
+diff = ImageChops.difference(img,img2)
+print(diff)
 #Reescalar imágenes y crear thumbnails
 #size = (200,200)
 #img3 = img.resize(size)
