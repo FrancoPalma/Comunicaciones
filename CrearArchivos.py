a45035e29c4708b068e8386393a23a12ffc171ae
Francoco97 <fpalmatrejo@gmail.com>
2020-06-06 17:45:04 -0400

    hecho

--- CrearArchivos.py
+++ CrearArchivos.py
@@ -1,12 +1,19 @@
 import random
 import string
+from random import randrange
 p=1
 for i in range(30):
     palabra=""
-    for i in range(5):
-        palabra+=random.choice(string.ascii_lowercase)
-    palabra=palabra+palabra+palabra+palabra
-    file = open("/Github/Comunicaciones/ArchivosConPatron/archivo"+str(p)+".txt", "w")
+    n=randrange(3)+3
+    letras=""
+    for i in range(n):
+        letras+=random.choice(string.ascii_lowercase)
+    while len(palabra) < 20:
+        if (len(palabra)+len(letras)) <= 20:
+            palabra+=letras
+        if len(palabra)<20:
+            palabra+=random.choice(string.ascii_lowercase)
+    file = open("/Github/Comunicaciones/ArchivosConPatron/archivo_con_patron"+str(p)+".txt", "w")
     file.write(palabra)
     file.close()
     p+=1
