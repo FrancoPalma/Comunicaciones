fc7e6f79574eb81b4d33af5422a0e9b78aa4a106
dEmanu1001 <64873829+dEmanu1001@users.noreply.github.com>
2020-06-07 14:48:53 -0400

    dd

---
+++ crear_imagenes.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import sys
+from PIL import ImageChops
+import random
+import string
+import os
+p=1
+palabra = " "
+tiempo = ""
+'''
+for i in range(30):
+    file = open("/Github/Comunicaciones/Imagenes/jpg/datos_img"+str(p)+".txt", "w")
+    file.write(palabra)
+    file.close()
+    p+=1
+'''
+
+print (os.stat('img15.bmp').st_size)
+
+#Esto abre la imagen
+#img = Image.open("\GitHub\Comunicaciones\Imagenes\jpg\img"+str(p)".bmp")
+
+#Esto la convierte a otro formato png, jpeg, tiff
+#img.save("\GitHub\Comunicaciones\Imagenes\jpg\img"+str(p)".jpeg","jpeg")
