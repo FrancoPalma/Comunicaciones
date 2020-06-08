ff089f01eb8365bcf2684a7e676b43e8fa23385c
Francoco97 <fpalmatrejo@gmail.com>
2020-06-08 19:56:21 -0400

    .

--- leerArchivos.py
+++ leerArchivos.py
@@ -5,8 +5,8 @@ import test
 l2=[]
 aux = 1
 while(aux <= 30):
-    time.sleep(0.1)
     start_time = time.time()
+    time.sleep(0.1)
     l=[]
     file = open("ArchivosCP/archivo"+str(aux)+".txt", "r")
     x = ""
@@ -17,11 +17,10 @@ while(aux <= 30):
     decompressed = test.decompress(compressed)
     l.append (decompressed)
     aux += 1
-    l.append("--- %s seconds ---" % (time.time() - start_time))
+    l.append("--- %s seconds ---" % ((time.time() - start_time)-0.1))
     l2.append(l)
 f = open("/Github/Comunicaciones/ResultadosTestCP.txt", "w")
 for i in l2:
-    print(i)
     for e in i:
         f.write(str(e))
     f.write("\n")
@@ -30,8 +29,8 @@ f.close()
 l2=[]
 aux = 1
 while(aux <= 30):
-    time.sleep(5)
     start_time = time.time()
+    time.sleep(0.1)
     l=[]
     file = open("ArchivosSP/archivo"+str(aux)+".txt", "r")
     x = ""
@@ -42,11 +41,10 @@ while(aux <= 30):
     decompressed = test.decompress(compressed)
     l.append (decompressed)
     aux += 1
-    l.append("--- %s seconds ---" % (time.time() - start_time))
+    l.append("--- %s seconds ---" % ((time.time() - start_time)-0.1))
     l2.append(l)
 f = open("/Github/Comunicaciones/ResultadosTestSP.txt", "w")
 for i in l2:
-    print(i)
     for e in i:
         f.write(str(e))
     f.write("\n")
