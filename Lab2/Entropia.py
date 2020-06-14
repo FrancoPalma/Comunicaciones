b6a4268502a0e72628470491491b457aba6c60f9
Francoco97 <fpalmatrejo@gmail.com>
2020-06-14 19:59:35 -0400

    .

--- Lab2/Entropia.py
+++ Lab2/Entropia.py
@@ -1,15 +1,16 @@
 p=1
 l=[]
+file2 = open("Entropia/Entropia"+str(p)+".txt", "w")
 for i in range(30):
+    verificador=True
     file = open("Archivos/Archivo"+str(p)+".txt", "r")
+    file2.write("-----Entropa Archivo"+str(p)+"-----")
     texto = ""
     aux=""
     for i in file:
         texto += i;
     file.close()
     for i in texto:
-        aux+=bin(ord(i))
-    file2 = open("Codigos/Codigo"+str(p)+".txt", "w")
     file2.write(aux)
     p+=1
 file2.close()
