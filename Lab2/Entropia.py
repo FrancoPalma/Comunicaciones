4f4fd277fcf9bd405339e14900457d02924eb802
Francoco97 <fpalmatrejo@gmail.com>
2020-06-14 19:53:42 -0400

    .

--- Lab2/Entropia.py
+++ Lab2/Entropia.py
@@ -1,18 +1,15 @@
 p=1
+l=[]
 for i in range(30):
     file = open("Archivos/Archivo"+str(p)+".txt", "r")
     texto = ""
     aux=""
-    binario=""
     for i in file:
         texto += i;
     file.close()
     for i in texto:
         aux+=bin(ord(i))
-    for i in aux:
-        if i != 'b':
-            binario+=i
     file2 = open("Codigos/Codigo"+str(p)+".txt", "w")
-    file2.write(binario)
-    file2.close()
+    file2.write(aux)
     p+=1
+file2.close()
