649010c5b9d95b5be089264f29f7a4caacaa011e
Francoco97 <fpalmatrejo@gmail.com>
2020-06-14 20:58:37 -0400

    .

--- Lab2/codificacion.py
+++ Lab2/codificacion.py
@@ -13,6 +13,22 @@ for i in range(30):
         for i in range(len(aux)):
             if i > 3:
                 binario+=aux[i]
+        if ((int(binario[0])+int(binario[1])+int(binario[2]))%2) == 0:
+            binario+=str(0)
+        else:
+            binario+=str(1)
+        if ((int(binario[1])+int(binario[2])+int(binario[3]))%2) == 0:
+            binario+=str(0)
+        else:
+            binario+=str(1)
+        if ((int(binario[2])+int(binario[3])+int(binario[4]))%2) == 0:
+            binario+=str(0)
+        else:
+            binario+=str(1)
+        if ((int(binario[3])+int(binario[4])+int(binario[5]))%2) == 0:
+            binario+=str(0)
+        else:
+            binario+=str(1)
         file2.write(binario)
     file2.close()
     p+=1
